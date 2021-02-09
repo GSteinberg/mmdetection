c680ed1f1ee7ffe9617c276a6fa2be862813f75e
Gabriel Steinberg <gsteinb1@binghamton.edu>
2021-02-09 08:08:38 -0500

    Added preservation of file name

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -310,6 +310,7 @@ class BaseDetector(nn.Module, metaclass=ABCMeta):
         Returns:
             img (Tensor): Only if not `show` or `out_file`
         """
+        img_name = img
         img = mmcv.imread(img)
         img = img.copy()
         if isinstance(result, tuple):
