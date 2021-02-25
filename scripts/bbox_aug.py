64fb5012fc44d2c67c4e23d7a7d63a6220d737d9
Gabriel Steinberg <gsteinb1@binghamton.edu>
2021-02-25 08:35:15 -0500

    Outputted imgs
    Next need to construct new anns

--- scripts/bbox_aug.py
+++ scripts/bbox_aug.py
@@ -9,9 +9,9 @@ def parse_args():
     Parse input arguments
     """
     parser = argparse.ArgumentParser(description='Augment images')
-    parser.add_argument('--input_dirs', dest='input_dirs',
-                        help='directories to take input imgs and anns to split',
-                        nargs="+")
+    parser.add_argument('--input_dir', dest='input_dir',
+                        help='directory to take input imgs and anns to split',
+                        type=str)
     parser.add_argument('--output_dir', dest='output_dir',
                         help='directory to save augmented imgs and ann',
                         type=str)
@@ -39,46 +39,59 @@ def get_annot_for_img(img_name, annot):
     return bboxes
 
 
-def augment(input_dirs, output_dir):
-    # iterate through every input img directory
-    for dir_num, input_dir in enumerate(input_dirs):
-        # load coco annotations
-        ann_name = os.path.join(input_dir, "coco_annotation.json")
-        with open(ann_name) as json_file:
-            annot = json.load(json_file)
+def augment(input_dir, output_dir):
+    # load coco annotations
+    ann_name = os.path.join(input_dir, "coco_annotation.json")
+    with open(ann_name) as json_file:
+        annot = json.load(json_file)
 
-        # iterate through every image in input_dirs
-        for image in os.scandir(input_dir):
-            # only check images with correct extension
-            if not image.name.endswith(".tif"):
-                print('{} not being parsed - does not have .tif extension'.format(image.name))
-                continue
+    # iterate through every image in input_dirs
+    new_annot = []
+    for image in os.scandir(input_dir):
+        # only check images with correct extension
+        if not image.name.endswith(".tif"):
+            print('{} not being parsed - does not have .tif extension'.format(image.name))
+            continue
 
-            # load image
-            img = cv2.imread(image.path)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # load image
+        img = cv2.imread(image.path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            # get corresponding annotation
-            bboxes = get_annot_for_img(image.name, annot)
+        # get corresponding annotation
+        bboxes = get_annot_for_img(image.name, annot)
 
-            # create transform object
-            transform = A.Compose([
-                A.RandomCrop(width=450, height=450),
-                A.HorizontalFlip(p=0.5),
-                A.RandomBrightnessContrast(p=0.2)
-            ],
-                bbox_params=A.BboxParams(
-                    format='coco',
-                    min_area=600,
-                    min_visibility=0.4,
-                    label_fields=['class_labels']
-                )
+        # create transform object
+        transform = A.Compose([
+            A.RandomCrop(width=450, height=450),
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.2)
+        ],
+            bbox_params=A.BboxParams(
+                format='coco',
+                min_area=600,
+                min_visibility=0.4
             )
+        )
 
-            # do actual transformation
-            transformed = transform(image=img, bboxes=bboxes)
-            transformed_img = transformed['image']
-            transformed_bboxes = transformed['bboxes']
+        # do actual transformation
+        transformed = transform(image=img, bboxes=bboxes)
+        transformed_img = transformed['image']
+        transformed_bboxes = transformed['bboxes']
+
+        # image output
+        output_img_name = os.path.join(output_dir, "Aug_" + image.name)
+        cv2.imwrite(output_img_name, transformed_img)
+
+        # recompile coco anns
+        new_annot.append(transformed_bboxes)
+
+    # constructing new annot
+    
+
+    # annotation output
+    output_ann_name = os.path.join(output_dir, "coco_annotation.json")
+    with open(output_ann_name, 'w') as outfile:
+        json.dump(new_annot, outfile)
 
 
 if __name__ == '__main__':
@@ -88,4 +101,4 @@ if __name__ == '__main__':
     print("Called with args:")
     print(args)
 
-    augment(args.input_dirs, args.output_dir)
+    augment(args.input_dir, args.output_dir)
