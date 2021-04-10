6ee3e7c0b3f7e3d77a490f63067ef495e8f427e8
Gabriel Steinberg <gsteinb1@binghamton.edu>
2021-04-10 11:21:20 -0400

    Removed old comments in lasso script

--- scripts/lasso.py
+++ scripts/lasso.py
@@ -32,29 +32,6 @@ for curr_dir, subdirs, files in os.walk('./'):
         mask = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
         img = img * mask2[:,:,np.newaxis]       # multiple by original image
         
-        # middle box fix
-        # xmid = width / 2
-        # ymid = height / 2
-
-        # xmin = math.floor(xmid - MID_BOX)
-        # xmax = math.floor(xmid + (MID_BOX-1) + 1*(xmid%1 > 0))
-        # ymin = math.floor(ymid - MID_BOX)
-        # ymax = math.floor(ymid + (MID_BOX-1) + 1*(ymid%1 > 0))
-
-        # if not any(any(row[xmin:xmax+1]) for row in mask[ymin:ymax+1]):
-        #     # make center box
-        #     for row in range(height):
-        #         for col in range(width):
-        #             if xmin <= col <= xmax and ymin <= row <= ymax: mask[row][col] = 1
-
-        # # mode: whether we are drawing rectangle or final touchup strokes
-        # mode = cv2.GC_INIT_WITH_MASK
-        # mask, bgdModel, fgdModel = cv2.grabCut(img, mask, None, bgdModel, fgdModel, 5, mode)
-
-        # 0 and 2-pixels put to 0 (ie bg)
-        # 1 and 3-pixels put to 1 (ie fg)
-        # mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
-
         # save image
         new_name = '{}/{}_Lasso.tif'.format(curr_dir, img_name.split('.')[0])
         cv2.imwrite(new_name, img)
