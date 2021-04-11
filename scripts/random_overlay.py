6e839537d875ef75b96b1fd5f86a146e3b5aa3f4
Gabriel Steinberg <gsteinb1@binghamton.edu>
2021-04-11 10:52:24 -0400

    Closert to completing random overlay
    Images too big, pfms not transparent but other trans imags work

--- scripts/random_overlay.py
+++ scripts/random_overlay.py
@@ -6,15 +6,39 @@
 from os import listdir, scandir
 from os.path import isfile, join
 import sys
-import json
+import random
+
+from PIL import Image
 
 OBJ_DIR = sys.argv[1]
 ORTHO_DIR = sys.argv[2]
+OUT_DIR = sys.argv[3]
 NUM_PFM = 26
 NUM_KSF = 6
 
-pfm_files = [f for f in listdir(OBJ_DIR) if isfile(join(OBJ_DIR, f)) and 'PFM' in f]
-ksf_files = [f for f in listdir(OBJ_DIR) if isfile(join(OBJ_DIR, f)) and 'KSF' in f]
+pfm_imgs = [OBJ_DIR+f for f in listdir(OBJ_DIR) if isfile(join(OBJ_DIR, f)) and 'PFM' in f]
+ksf_imgs = [OBJ_DIR+f for f in listdir(OBJ_DIR) if isfile(join(OBJ_DIR, f)) and 'KSF' in f]
 
 for ortho in scandir(ORTHO_DIR):
-    
+    # select correct amounts of pfm and ksf
+    objects = random.sample(pfm_imgs, NUM_PFM) + random.sample(ksf_imgs, NUM_KSF)
+
+    # open orthophoto
+    bg = Image.open(ortho.path)
+    bg_wid, bg_hei = bg.size
+
+    # loop through each pfm-1
+    for obj in objects:
+        fg = Image.open(obj)
+        fg_wid, fg_hei = fg.size
+
+        # get x and y coordinates for top left pixel of pfm
+        x_lim, y_lim = bg_wid-fg_wid, bg_hei-fg_hei
+        tl_x, tl_y = random.randint(0,x_lim), random.randint(0,y_lim)
+
+        # overlay
+        bg.paste(fg, (tl_x,tl_y), mask=fg.convert('RGBA'))
+        break
+
+    import pdb;pdb.set_trace()
+    bg.save( join(OUT_DIR, ortho.name) )
