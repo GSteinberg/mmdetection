99a9992dfa992540a47ed6e2fcb0ee12e73e9eb8
Gabriel Steinberg <gsteinb1@binghamton.edu>
2021-04-13 11:21:07 -0400

    Added restricting box

--- scripts/random_overlay.py
+++ scripts/random_overlay.py
@@ -16,6 +16,12 @@ OUT_DIR = sys.argv[3]
 NUM_PFM = 26
 NUM_KSF = 6
 
+# box inside which mines should go
+orth_tl = (2803, 1329)
+orth_tr = (8900, 4989)
+orth_bl = (1088, 4325)
+orth_br = (7253, 7937)
+
 pfm_imgs = [OBJ_DIR+f for f in listdir(OBJ_DIR) if isfile(join(OBJ_DIR, f)) and 'PFM' in f]
 ksf_imgs = [OBJ_DIR+f for f in listdir(OBJ_DIR) if isfile(join(OBJ_DIR, f)) and 'KSF' in f]
 
@@ -25,20 +31,22 @@ for ortho in scandir(ORTHO_DIR):
 
     # open orthophoto
     bg = Image.open(ortho.path)
-    bg_wid, bg_hei = bg.size
+    # bg_wid, bg_hei = bg.size
+    bg_wid, bg_hei = 6788, 5064
+    bg_x, bg_y = 1136, 1378
 
     # loop through each pfm-1
     for obj in objects:
+        print('Overlaying {} onto {}'.format(obj.split('/')[-1], ortho.name))
+        
         fg = Image.open(obj)
         fg_wid, fg_hei = fg.size
 
         # get x and y coordinates for top left pixel of pfm
-        x_lim, y_lim = bg_wid-fg_wid, bg_hei-fg_hei
-        tl_x, tl_y = random.randint(0,x_lim), random.randint(0,y_lim)
+        x_lim, y_lim = bg_wid-fg_wid, bg_hei-fg_hei 
+        tl_x, tl_y = random.randint(bg_x, x_lim), random.randint(bg_y, y_lim)
 
         # overlay
         bg.paste(fg, (tl_x,tl_y), mask=fg.convert('RGBA'))
-        break
 
-    import pdb;pdb.set_trace()
     bg.save( join(OUT_DIR, ortho.name) )
